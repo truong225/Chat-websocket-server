2b8530d285d96db882cf44b853a501855cfb214d
truong225 <truongmero225@gmail.com>
2018-06-13 10:07:06 +0700

    * fix login issues

--- front-end/chat-client-react/src/SignIn.js
+++ front-end/chat-client-react/src/SignIn.js
@@ -1,17 +1,18 @@
 import React, {Component} from "react";
+import axios from 'axios'
 
 export default class SingIn extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            user: '',
+            username: '',
             password: ''
         };
     }
 
     handleUserChange(e) {
-        this.setState({ user: e.target.value })
+        this.setState({ username: e.target.value })
     }
     
     handlePasswordChange(e) {
@@ -19,32 +20,37 @@ export default class SingIn extends Component {
     }
 
     signIn() {
-        var ajax = new XMLHttpRequest();
-        var url = "http://localhost:8080/test/u=" + this.state.user + "/pass=" + this.state.password;
-        ajax.onreadystatechange = this.show(this,ajax.responseText);
-        ajax.open("GET", url, true);
-        ajax.send();
+        if(this.state.username === '' || this.state.password === ''){
+            alert('Username or password is empty');
+            return;
+        }
+
+        var messagePayload ="http://localhost:8080/signin/u="+this.state.username+"/pass="+this.state.password;
+        axios.get(messagePayload).then(function(response){
+            this.showMessageboard(response)
+        }.bind(this))
     }
 
-    show(e, response) {
-        if(response !== '' || response !== null)
-            e.props.onClickAdd();
+    showMessageboard(response) {
+        if(response.data==='')
+                alert('No username or password')
+        else
+            this.props.onClickAdd()
     }
+
 
     render() {
         return (
             <div className="content">
                 <form className="form-signup">
-                    <h2 className="form-signup-heading"> Please sign up </h2>
-                    <label htmlFor="inputUser" className="sr-only"> User Name </label>
-                    <input type="user" onChange={this.handleUserChange.bind(this)} id="inputUser" className="form-control" placeholder="User name" required autoFocus/>
-                    <br/>
+                    <h2 className="form-signup-heading">Please sign up</h2>
+                    <label htmlFor="inputUser" className="sr-only">User Name</label>
+                    <input type="user" onChange={this.handleUserChange.bind(this)} id="inputUser" className="form-control" placeholder="User name" required autoFocus />
                     <label htmlFor="inputPassword" className="sr-only"> Password </label>
                     <input type="password" onChange={(e) => {
                         this.handlePasswordChange(e);
                     }} id="inputPassword" className="form-control" placeholder="Password" required />
-                    <br/>
-                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn.bind(this)} type="button" > Sign in </button>
+                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn.bind(this)} type="button"> Sign in </button>
                 </form>
             </div>
 
--- src/main/java/com/ifi/chat/stomp/controller/UserController.java
+++ src/main/java/com/ifi/chat/stomp/controller/UserController.java
@@ -3,7 +3,6 @@ package com.ifi.chat.stomp.controller;
 import com.ifi.chat.entity.User;
 import com.ifi.chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
