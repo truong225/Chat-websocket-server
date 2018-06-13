bb356220b963b664c659cd9aff08fb91bab0e362
truong225 <truongmero225@gmail.com>
2018-06-13 09:25:43 +0700

    + add new branch

--- front-end/chat-client-react/src/SignIn.js
+++ front-end/chat-client-react/src/SignIn.js
@@ -1,33 +1,12 @@
 import React, {Component} from "react";
-import SocketConnection from './SocketConnection'
 
 export default class SingIn extends Component {
     constructor(props) {
         super(props);
 
-        var stomp = SocketConnection.stomp;
-        stomp.connect({}
-            , function onConnect(data) {
-                stomp.subscribe("/public/login", function onReceive(message){
-                    const receivedMessage = JSON.parse(message);
-                if (receivedMessage.type === 'error') {
-                    console.log('hello')
-                }
-                else {
-                    this.props.onClickAdd();
-                    alert('Hello')
-                }
-            })
-                ;
-            }
-            , function onError(error) {
-                alert("WebSocket error: " + error);
-            });
-
         this.state = {
             user: '',
-            password: '',
-            stompClient: stomp
+            password: ''
         };
     }
 
@@ -40,11 +19,16 @@ export default class SingIn extends Component {
     }
 
     signIn() {
-        var messagePayload = {
-            "username": this.state.user,
-            "password": this.state.password
-        }
-        this.state.stompClient.send("/app/chat/login", {}, JSON.stringify(messagePayload));
+        var ajax = new XMLHttpRequest();
+        var url = "http://localhost:8080/test/u=" + this.state.user + "/pass=" + this.state.password;
+        ajax.onreadystatechange = this.show(this,ajax.responseText);
+        ajax.open("GET", url, true);
+        ajax.send();
+    }
+
+    show(e, response) {
+        if(response !== '' || response !== null)
+            e.props.onClickAdd();
     }
 
     render() {
@@ -53,14 +37,14 @@ export default class SingIn extends Component {
                 <form className="form-signup">
                     <h2 className="form-signup-heading"> Please sign up </h2>
                     <label htmlFor="inputUser" className="sr-only"> User Name </label>
-                    <input type="user" onChange={this.handleUserChange.bind(this)} id="inputUser" className="form-control" placeholder="User name" required autoFocus />
+                    <input type="user" onChange={this.handleUserChange.bind(this)} id="inputUser" className="form-control" placeholder="User name" required autoFocus/>
                     <br/>
                     <label htmlFor="inputPassword" className="sr-only"> Password </label>
                     <input type="password" onChange={(e) => {
                         this.handlePasswordChange(e);
                     }} id="inputPassword" className="form-control" placeholder="Password" required />
                     <br/>
-                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button" > Sign in </button>
+                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn.bind(this)} type="button" > Sign in </button>
                 </form>
             </div>
 
--- src/main/java/com/ifi/chat/stomp/controller/MessageController.java
+++ src/main/java/com/ifi/chat/stomp/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.ifi.chat.stomp.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,6 +11,12 @@ public class MessageController {
     @MessageMapping("/chat/sendMessage")
     @SendTo("/public/chat")
     public String sendMessage(@Payload String message) {
+        return message;
+    }
+
+    @MessageMapping("/chat/u={username}")
+    @SendTo("/public/chat/{username}")
+    public String sendMessageToUser(@Payload String message, @DestinationVariable String username){
         return message;
     }
 }
